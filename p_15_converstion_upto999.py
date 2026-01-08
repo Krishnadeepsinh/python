34ee330c1d7f0e60b3e2fbe8598eab035f2643ce
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-08 23:03:32 +0530

    Add files via upload

---
+++ p_15_converstion_upto999.py
@@ -0,0 +1,36 @@
+#write a program to convert given 3 digit amount into words
+# input : 175 output : one hundred seventy five
+
+#Getting amount
+amount = input("Enter 3 digit amount: ")
+#Converting into int
+amount = int(amount)
+
+#Getting number into 1 and the last 2 parts
+frist = amount // 100
+last = amount % 100
+
+hundreds = ['', 'one hundred', 'two hundred', 'three hundred', 'four hundred',
+            'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred']
+
+unit = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
+         'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
+         'sixteen', 'seventeen', 'eighteen', 'nineteen',
+         'twenty', 'twenty one', 'twenty two', 'twenty three', 'twenty four',
+         'twenty five', 'twenty six', 'twenty seven', 'twenty eight', 'twenty nine',
+         'thirty', 'thirty one', 'thirty two', 'thirty three', 'thirty four',
+         'thirty five', 'thirty six', 'thirty seven', 'thirty eight', 'thirty nine',
+         'forty', 'forty one', 'forty two', 'forty three', 'forty four',
+         'forty five', 'forty six', 'forty seven', 'forty eight', 'forty nine',
+         'fifty', 'fifty one', 'fifty two', 'fifty three', 'fifty four',
+         'fifty five', 'fifty six', 'fifty seven', 'fifty eight', 'fifty nine',
+         'sixty', 'sixty one', 'sixty two', 'sixty three', 'sixty four',
+         'sixty five', 'sixty six', 'sixty seven', 'sixty eight', 'sixty nine',
+         'seventy', 'seventy one', 'seventy two', 'seventy three', 'seventy four',
+         'seventy five', 'seventy six', 'seventy seven', 'seventy eight', 'seventy nine',
+         'eighty', 'eighty one', 'eighty two', 'eighty three', 'eighty four',
+         'eighty five', 'eighty six', 'eighty seven', 'eighty eight', 'eighty nine',
+         'ninety', 'ninety one', 'ninety two', 'ninety three', 'ninety four',
+         'ninety five', 'ninety six', 'ninety seven', 'ninety eight', 'ninety nine']
+
+print(hundreds[frist], unit[last])
