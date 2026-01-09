c7fa0ff380bb2276dd5f7231b53dd2937b5017ee
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-09 11:33:10 +0530

    Add files via upload

---
+++ p_16_relation_operater.py
@@ -0,0 +1,24 @@
+ #WAP FOR PERFORM ALL THE RELATIONAL OPERATOR
+ #NAME : KRISHNADEEPSINH
+ #DATE:9/1/26
+
+num1=int(input("ENTER NO 1 : "))
+num2=int(input("ENTER NO 2 : "))
+
+result = num1 == num2   #False
+print(f"{result} = {num1} == {num2}")
+
+result = num1 != num2   #True
+print(f"{result} = {num1} != {num2}")
+
+result=num1 < num2      #True
+print(f"{result} = {num1} < {num2}")
+
+result=num1 > num2      #False
+print(f"{result} = {num1} > {num2}")
+
+result=num1 <= num2     #True
+print(f"{result} = {num1} <= {num2}")
+
+result=num1 >= num2     #False
+print(f"{result} = {num1} >= {num2}")
