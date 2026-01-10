b8aefb496197a99f7f2c698222b53f4cbff7bb4c
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-10 12:09:29 +0530

    Add files via upload

---
+++ TASK/p_10_TASK.py
@@ -0,0 +1,9 @@
+#10. Given a list of numbers, sort it in ascending and descending order.
+#NAME=KRISHNADEEPSINH
+#DATE=10/1/26
+
+list1=[1,2,3,4,5,6,7,8,9,10]
+list2=[10,9,8,7,6,5,4,3,2,1]
+
+print("ASCENDING ORDER",sorted(list2))
+print("ASCENDING ORDER",sorted(list1,reverse=True))
