5b63f777f1475a0d65f1d8052b5055e5246c6593
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-10 11:04:25 +0530

    Add files via upload

---
+++ TASK/p_1_TASK.py
@@ -0,0 +1,8 @@
+#1. Create a list of 10 numbers and find the sum using built-in functions.
+#NAME=KRISHNADEEPSINH
+#DATE=10/1/26
+
+list=[1,2,3,4,5,6,7,8,9,10]
+
+
+print(sum(list))
---
+++ TASK/p_2_TASK.py
@@ -0,0 +1,7 @@
+#2. Given a list of numbers, find the maximum and minimum values.
+#NAME=KRISHNADEEPSINH
+#DATE=10/1/26
+
+list=[10,20,55,45,98,54,32,12]
+print("MAXIMUM NUMBERS IS :",max(list))
+print("MINIMUN NUMBERS IS :",min(list))
