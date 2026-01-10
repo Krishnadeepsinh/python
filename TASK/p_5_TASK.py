fe4cf82f4127e851b8596a75731845f941e69a91
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-10 11:34:43 +0530

    Add files via upload

---
+++ TASK/p_5_TASK.py
@@ -0,0 +1,7 @@
+#5. Count how many times a specific value appears in a list using built-in methods.
+##NAME=KRISHNADEEPSINH
+#DATE=10/1/26
+
+
+list1=[1,2,3,4,5,6,7,8,9,10,0,11,1,1,2]
+print(list1.count(1))
