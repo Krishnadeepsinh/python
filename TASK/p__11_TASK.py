c4b9cc9c0c1902099c3f25cd6bc131b283fb4a34
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-11 20:13:49 +0530

    Update p__11_TASK.py

--- TASK/p__11_TASK.py
+++ TASK/p__11_TASK.py
@@ -1,6 +1,7 @@
 #11. Create a dictionary of 5 students with their marks and find the total marks.
 #NAME=KRISHNADEEPSINH
-#DATE=3/1/26
+#DATE=11/1/26
 
 stud={'KRISHNADEEPSINH':100,'YAGNADEEPSINH':90,'VIVEK R':70,'TUSHAR':80,'VIVEK S':81}
+
 print(sum(stud.values()))
