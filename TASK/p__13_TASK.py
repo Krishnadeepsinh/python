1c638dfdfa0b7f09d434c0806acb1a365e5d0590
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-11 20:51:32 +0530

    Add files via upload

---
+++ TASK/p__13_TASK.py
@@ -0,0 +1,6 @@
+#13. From a dictionary, extract only the values into a list.
+#NAME=KRISHNADEEPSINH
+#DATE=11/1/26
+
+dic={'NAME':'KRISHNADEEPSINH',"AGE":19,'GENDER':'MALE','DOB':'2-MAY-2006','HOBBIES':'READING'}
+print(dic.values())
