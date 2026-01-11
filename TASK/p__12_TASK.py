fb749c0df9a4fae91fb574b31948d7d5f49b05ac
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-11 20:17:26 +0530

    Add files via upload

---
+++ TASK/p__12_TASK.py
@@ -0,0 +1,6 @@
+#12. From a dictionary, extract only the keys into a list.
+#NAME=KRISHNADEEPSINH
+#DATE=11/1/26
+
+dic={'NAME':'KRISHNADEEPSINH',"AGE":19,'GENDER':'MALE','DOB':'2-MAY-2006','HOBBIES':'READING'}
+print(dic.keys())
