faa8ed86c25b58c4d224648e9114137d34a878cd
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-19 12:05:12 +0530

    Update p__1_time_con_24to12.py

--- exercise/p__1_time_con_24to12.py
+++ exercise/p__1_time_con_24to12.py
@@ -15,10 +15,11 @@ time=int(input("ENTER THE TIME IN HOUR FROM 1 TO 24 : "))
 am='am'
 pm='pm'
 
-if time<=12:
+if time>0 and time<=12:
        print("TIME IN HOURS IS : ",time," ",am)
 if time>12 and time<=24:
        print("TIME IN HOURS IS : ",time-12," ",pm)
 if time>24:
        print('INVAILD TIME ')
+
        
