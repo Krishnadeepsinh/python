d1346685884346a5f748a1a4f57dae52144a822d
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-20 11:44:16 +0530

    Add files via upload

---
+++ exercise/p__5_age_compare.py
@@ -0,0 +1,9 @@
+#5) write a program to find out elder brother from given two brother's age. 
+
+brother1=int(input("ENTER THE AGE OF BROTHER 1 : "))
+brother2=int(input("ENTER THE AGE OF BROTHER 2 : "))
+
+if brother1>brother2:
+    print("BROTHER 1 IS ELDER & BROTHER 2 IS OLDER")
+else:
+    print("BROTHER 2 IS ELDER & BROTHER 1 IS OLDER")
