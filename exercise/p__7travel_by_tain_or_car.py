d821bed6b1e994e7b03158bd1372ebf75ea15e76
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-20 11:56:18 +0530

    Add files via upload

---
+++ exercise/p__7travel_by_tain_or_car.py
@@ -0,0 +1,10 @@
+#7) write a program to decide which is cheaper approach to go from ahmedabad to delhi. by car or by train. consider person has his own petrol car  and he prefer to travel by 1st class train 
+
+
+car=(986/20)*96   #(kilometer/mileage)*litre_price
+train=3700
+
+if bycar<bytrain:
+    print("car is cheaper")
+else:
+    print("train is cheaper")
