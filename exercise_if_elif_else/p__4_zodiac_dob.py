97bb375ab51c3ad70b16f9bfabf268606da22e4a
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-21 12:06:37 +0530

    Update p__4_zodiac_dob.py

--- exercise_if_elif_else/p__4_zodiac_dob.py
+++ exercise_if_elif_else/p__4_zodiac_dob.py
@@ -17,44 +17,45 @@
 dob_date=int(input("ENTER THE DATE OF BIRTH"))
 dob_month=int(input("ENTER THE BIRTH MONTH : "))
 
-if (dob_date>21 and dob_month==3) or (dob_date<19 and dob_month==4) :
+if (dob_date>=21 and dob_month==3) or (dob_date<=19 and dob_month==4) :
     print("ARIES")
 
-elif (dob_date>20 and dob_month==4) or (dob_date<20 and dob_month==5) :
+elif (dob_date>=20 and dob_month==4) or (dob_date<=20 and dob_month==5) :
     print("TAURUS")
 
-elif (dob_date>21 and dob_month==5) or (dob_date<21 and dob_month==6) :
+elif (dob_date>=21 and dob_month==5) or (dob_date<=21 and dob_month==6) :
     print("GEMINI")
 
-elif (dob_date>22 and dob_month==6) or (dob_date<22 and dob_month==7) :
+elif (dob_date>=22 and dob_month==6) or (dob_date<=22 and dob_month==7) :
     print("CANCER")
 
-elif (dob_date>23 and dob_month==7) or (dob_date<22 and dob_month==8) :
+elif (dob_date>=23 and dob_month==7) or (dob_date<=22 and dob_month==8) :
     print("LEO")
 
-elif (dob_date>23 and dob_month==8) or (dob_date<22 and dob_month==9) :
+elif (dob_date>=23 and dob_month==8) or (dob_date<=22 and dob_month==9) :
     print("VIRGO")
 
-elif (dob_date>23 and dob_month==9) or (dob_date<22 and dob_month==10) :
+elif (dob_date>=23 and dob_month==9) or (dob_date<=22 and dob_month==10) :
     print("LIBRA")
 
-elif (dob_date>24 and dob_month==10) or (dob_date<21 and dob_month==11) :
+elif (dob_date>=24 and dob_month==10) or (dob_date<=21 and dob_month==11) :
     print("SCORPIO")
 
-elif (dob_date>22 and dob_month==11) or (dob_date<21 and dob_month==12) :
+elif (dob_date>=22 and dob_month==11) or (dob_date<=21 and dob_month==12) :
     print("SAGITTARIUS")
 
-elif (dob_date>22 and dob_month==12) or (dob_date<19 and dob_month==1) :
+elif (dob_date>=22 and dob_month==12) or (dob_date<=19 and dob_month==1) :
     print("CAPRICORN")
 
 
-elif (dob_date>20 and dob_month==1) or (dob_date<18 and dob_month==2) :
+elif (dob_date>=20 and dob_month==1) or (dob_date<=18 and dob_month==2) :
     print("AQUARIUS")
 
 
-elif (dob_date>19 and dob_month==2) or (dob_date<20 and dob_month==3) :
+elif (dob_date>=19 and dob_month==2) or (dob_date<=20 and dob_month==3) :
     print("AQUARIUS")
 
 
 else:
+
     print("INVAILD DOB ")
