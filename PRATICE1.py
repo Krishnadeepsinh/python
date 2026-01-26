52baea8b034c44da6b4a1cfcc20c9c4d3977c9b4
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-26 08:59:38 +0530

    Fix formatting and add new lines in PRATICE1.py

--- PRATICE1.py
+++ PRATICE1.py
@@ -1,6 +1,7 @@
-#Create a list and find the position (index) of a given element.
+ #Create a list and find the position (index) of a given element.
 no =[1,2,3,4,5,6,7,8,9,10]
 
 check=int(input("Enter the element whose index u want :"))
+
 
 print(f"The index of {check} is : {no.index(check)}")
