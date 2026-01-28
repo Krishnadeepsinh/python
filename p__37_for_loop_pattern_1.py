b1415d885741cc20a9cd815e7279064f954e8627
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-28 11:40:11 +0530

    Add files via upload

---
+++ p__37_for_loop_pattern_1.py
@@ -0,0 +1,12 @@
+'''
+Docstring for p__36_for_loop_pattern_1
+*
+* *
+* * * 
+* * * *
+* * * * *
+'''
+for row in range(1,6):
+    for astreik in range(1,row+1):
+        print('*',end=" ")
+    print(" ")
---
+++ p__38_for_loop_pattern_2.py
@@ -0,0 +1,14 @@
+'''
+Docstring for p__37_for_loop_pattern_2
+
+* * * * *
+* * * *
+* * * 
+* *
+*
+
+'''
+for row in range(6,1,-1):
+    for astriek in range(1,row):
+        print('*',end=' ')
+    print(' ')
