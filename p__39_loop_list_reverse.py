d637115fedc3f0e2e54f52f1bcc9243447d28187
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-29 12:25:53 +0530

    Add files via upload

---
+++ p__39_loop_list_reverse.py
@@ -0,0 +1,6 @@
+# write a program to display list in reverse 
+cars = ["Maruti Suzuki","Hyundai","Tata Motors","Mahindra","Toyota","Honda","Kia","Skoda","Volkswagen","Renault"]
+print(cars)
+
+for item in reversed(cars):
+    print(item)
---
+++ p__40_udf_square&cube.py
@@ -0,0 +1,19 @@
+# concept of user defined function
+#syntax 
+#def function-name(parameters):
+#example 
+
+def getSquare(number): 
+    square= number * number
+    return square
+
+def getCube(num):
+    return getSquare(num) * num
+
+
+n1 = int(input("ENTER NUMBER TO FIND SQAURE & CUBE : "))
+result=getSquare(n1)
+print(f"CUBE OF {n1} IS : {result}")
+
+result1=getCube(n1)
+print(f"CUBE OF {n1} IS : {result1}")
