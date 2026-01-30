f5fa259dfb2743fb64c4d3af4dda855513d364ec
Krishnadeep Chudasama <chudasamakrishnadeep22@gmail.com>
2026-01-30 11:53:52 +0530

    Add files via upload

---
+++ p__3_udf.py
@@ -0,0 +1,20 @@
+import shutil as s
+width=s.get_terminal_size().columns
+# 1	 Without return value without argument
+def getprintline():
+    print('_'*width)
+
+# 2	Without return value with argument 
+def printmessage(msg):
+    print(msg.center(width))
+
+# 3	 With return value without argument
+def pi():
+    pi=22/7
+    return pi 
+
+getprintline()
+printmessage("KRISHNADEEPSINH")
+getprintline()
+pi=pi()
+print('pi=',pi)
